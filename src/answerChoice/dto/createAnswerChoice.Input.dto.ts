3fb02f4af859e0a01149b6556110698044a52677
chie-hira <capella.a36@gmail.com>
2024-08-24 11:29:51 +0900

    バリデーション追加
    バリデーターをインストール
    $ npm i class-validator class-transformer

--- src/answerChoice/dto/createAnswerChoice.Input.dto.ts
+++ src/answerChoice/dto/createAnswerChoice.Input.dto.ts
@@ -1,8 +1,10 @@
 import { Field, InputType, Int } from '@nestjs/graphql';
+import { IsNotEmpty } from 'class-validator';
 
 @InputType()
 export class CreateAnswerChoiceInput {
   @Field()
+  @IsNotEmpty()
   choice: string;
 
   @Field(() => Int)
--- src/question/dto/createQuestion.Input.dto.ts
+++ src/question/dto/createQuestion.Input.dto.ts
@@ -1,12 +1,15 @@
 import { Field, InputType, Int } from '@nestjs/graphql';
 import { AnswerFormat } from '../enums/question.enum';
+import { IsNotEmpty } from 'class-validator';
 
 @InputType()
 export class CreateQuestionInput {
   @Field()
+  @IsNotEmpty()
   title: string;
 
   @Field()
+  @IsNotEmpty()
   answerFormat: AnswerFormat;
 
   @Field(() => Int)
