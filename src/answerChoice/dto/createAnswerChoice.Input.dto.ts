1c8a87fea44a0d14b7e71d1d6228a0133fd162dd
chie-hira <capella.a36@gmail.com>
2024-08-26 22:25:47 +0900

    createAnswerChoice修正

--- src/answerChoice/dto/createAnswerChoice.Input.dto.ts
+++ src/answerChoice/dto/createAnswerChoice.Input.dto.ts
@@ -1,4 +1,4 @@
-import { Field, InputType, Int } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
 import { IsNotEmpty } from 'class-validator';
 
 @InputType()
@@ -6,7 +6,4 @@ export class CreateAnswerChoiceInput {
   @Field()
   @IsNotEmpty()
   answerChoice: string;
-
-  @Field(() => Int)
-  questionId: number;
 }
